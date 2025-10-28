401b151a81fbcb50d8e26a3ef6b58c6ab5f6ec34
Marcio <marcio.piva@eficazmarketing.com>
2025-10-28 15:40:08 -0300

    fix: test

--- src/app/api/sorteio/route.ts
+++ src/app/api/sorteio/route.ts
@@ -1,9 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
-const prisma = new PrismaClient();
-
 export const runtime = "nodejs";
+
+const prisma = new PrismaClient();
 
 export async function POST(req: Request) {
   try {
